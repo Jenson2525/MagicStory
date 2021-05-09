c6e24a7a8a7d3987f7c2b7a4e837154f365883b0
JensonXu-CN <55434673+Jenson2525@users.noreply.github.com>
2021-05-09 13:41:09 +0800

    Create Bread.java

---
+++ MagicStory/Bread.java
@@ -0,0 +1,11 @@
+package MagicStory;
+
+public class Bread {
+    private final int energy;
+    public Bread(int energy){
+        this.energy = energy;
+    }
+    public int GetEnergy(){
+        return this.energy;
+    }
+}
